10ce12b06d62c806f02b5880dd7301feccc8b7ba
Vitor Rosa <vitorosa306@gmail.com>
2020-05-23 14:45:04 -0300

    Funçao Traduzir adicionada

--- calculator/menu.rb
+++ calculator/menu.rb
@@ -10,6 +10,7 @@ module Calculator
       puts "1. Média Preconceituosa"
       puts "2. Calculadora sem números"
       puts "3. Filtrar Filmes"
+      puts "4. Tradutor"
       puts "0. Sair"
       puts ""
       print "Sua opção: "
@@ -47,6 +48,20 @@ module Calculator
         year = gets.chomp
 
         puts operation.filter_films(genres, year)
+
+        puts "Aperte enter pra continuar"
+        enter = gets.chomp
+        system "clear || cls"
+      when 4
+        print "Insira o texto a ser traduzido: "
+        text = gets.chomp
+        puts "Insira a língua original do texto (Ex: pt, en, es)"
+        lang = gets.chomp
+        lang += "-"
+        puts "Insira a língua para qual o texto vai ser traduzido (Ex: pt, en, es)"
+        lang += gets.chomp
+
+        puts operation.translate(text, lang)
 
         puts "Aperte enter pra continuar"
         enter = gets.chomp
--- extra_operations.rb
+++ extra_operations.rb
@@ -1,2 +1,12 @@
 module ExtraOperations
+  def translate(text, lang)
+    
+    uri = URI('https://translate.yandex.net/api/v1.5/tr.json/translate')
+    key = "trnsl.1.1.20200415T041342Z.a521d34ac9ebf42c.3581a4916fd3200438f3be1befbeb150718c3634"
+    params = { :key => key, :text => text, :lang => lang }
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    res = JSON.parse(res.body)
+    return res["text"]
+  end
 end
